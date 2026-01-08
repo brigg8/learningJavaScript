fea763304156eb27000dd259eff4e5a471717876
Brandon Riggione <bpr008@bucknell.edu>
2026-01-08 12:39:50 -0500

    learning Math class

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-// COUNTER PROGRAM
+// Math = built-in objects that provides a 
+// collection of properties and methods
 
-const decreaseBtn = document.getElementById("decreaseBtn");
-const resetBtn = document.getElementById("resetBtn");
-const increaseBtn = document.getElementById("increaseBtn");
-const countLabel = document.getElementById("countLabel");
-let count = 0;
 
-increaseBtn.onclick = function(){
-    count++;
-    countLabel.textContent = count;
-}
+let x = 3;
+let y = 2;
+let z = 1;
 
-decreaseBtn.onclick = function(){
-    count--;
-    countLabel.textContent = count;
-}
 
-resetBtn.onclick = function(){
-    count = 0;
-    countLabel.textContent = count;
-}
+// z = Math.round(x); rounds normally
+
+// z = Math.floor(x); rounds down
+
+// z = Math.ceil(x); rounds up
+
+// z = Math.trunc(x); elimatinates decimal portions
+
+// z = Math.pow(x, y); this is first number to the power of secound number
+
+// z = Math.sqrt(x); square root
+
+// z = Math.log(x); logorithmic
+
+// z = Math.sin(x); sin of a number needs to be in radians
+
+// z = Math.cos(x); 
+
+// z = Math.tan(x);
+
+// z = Math.abs(x); absolute value
+
+// z = Math.sign(x); finds the sign of a number
+
+let max = Math.max(x, y, z)
+let min = Math.min(x, y, z);
+console.log(min);
