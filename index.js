7dc5d92b9e96b83d89caa124771956a4943cb1d3
Brandon Riggione <bpr008@bucknell.edu>
2026-01-08 12:47:51 -0500

    learning random class

--- index.js
+++ index.js
@@ -1,36 +1,7 @@
-// Math = built-in objects that provides a 
-// collection of properties and methods
+// RANDOM NUMBER GENERATOR
 
+const min = 50;
+const max = 100;
+let randomNum = Math.floor(Math.random() * (max - min)) + min;
 
-let x = 3;
-let y = 2;
-let z = 1;
-
-
-// z = Math.round(x); rounds normally
-
-// z = Math.floor(x); rounds down
-
-// z = Math.ceil(x); rounds up
-
-// z = Math.trunc(x); elimatinates decimal portions
-
-// z = Math.pow(x, y); this is first number to the power of secound number
-
-// z = Math.sqrt(x); square root
-
-// z = Math.log(x); logorithmic
-
-// z = Math.sin(x); sin of a number needs to be in radians
-
-// z = Math.cos(x); 
-
-// z = Math.tan(x);
-
-// z = Math.abs(x); absolute value
-
-// z = Math.sign(x); finds the sign of a number
-
-let max = Math.max(x, y, z)
-let min = Math.min(x, y, z);
-console.log(min);
+console.log(randomNum);
