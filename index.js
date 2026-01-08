380eb81bf9fb1590d4df046e8a2ed49590b26123
Brandon Riggione <bpr008@bucknell.edu>
2026-01-08 17:25:34 -0500

    if statment practice

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-// RANDOM NUMBER GENERATOR
+// IF STATEMENTS = if a condition is true, execute some code
+//                 if not, do something else
+const myText = document.getElementById("myText");
+const mySubmit = document.getElementById("mySubmit");
+const resultElement = document.getElementById("resultElement");
+let age;
 
-const myButton = document.getElementById("myButton");
-const label1 = document.getElementById("label1");
-const label2 = document.getElementById("label2");
-const label3 = document.getElementById("label3");
-
-const min = 1;
-const max = 6;
-let randomNum1;
-let randomNum2;
-let randomNum3;
-
-
-myButton.onclick = function(){
-    randomNum1 = Math.floor(Math.random() * max) + min;
-    randomNum2 = Math.floor(Math.random() * max) + min;
-    randomNum3 = Math.floor(Math.random() * max) + min;
-    label1.textContent= randomNum1;
-    label2.textContent= randomNum2;
-    label3.textContent= randomNum3;
-
+mySubmit.onclick = function(){
+    age = myText.value;
+    age = Number(age);
+    if(age >= 100){
+        resultElement.textContent = `You are too old to enter this site`
+    }
+    else if(age == 0){
+        resultElement.textContent = `You cant enter. You were just born.`
+    }
+    else if(age >= 18){
+        resultElement.textContent = `You are old enough to enter this site`
+    }
+    else if(age < 0 ){
+        resultElement.textContent = `Your age can't be below 0`
+    }
+    else{
+        resultElement.textContent = `You must be 18+ to enter this site`
+    }
 }
