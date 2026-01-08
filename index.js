fb56eaa88baeb7313b2713f99b6032a952aa0d9a
Brandon Riggione <bpr008@bucknell.edu>
2026-01-08 17:47:34 -0500

    leanring radio buttons

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-// RANDOM NUMBER GENERATOR
+// checked = property that determines the checked state of an HTML checkbox or radio button element
 
-const myButton = document.getElementById("myButton");
-const label1 = document.getElementById("label1");
-const label2 = document.getElementById("label2");
-const label3 = document.getElementById("label3");
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const payPalBtn = document.getElementById("payPalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
 
-const min = 1;
-const max = 6;
-let randomNum1;
-let randomNum2;
-let randomNum3;
+mySubmit.onclick = function(){
+    if(myCheckBox.checked){
+        subResult.textContent = `You are subscribed!`;
+    }
+    else{
+        subResult.textContent = `You are not subscribed!`;
+    }
 
+    if(visaBtn.checked){
+        paymentResult.textContent = `You are paying with visa`;
 
-myButton.onclick = function(){
-    randomNum1 = Math.floor(Math.random() * max) + min;
-    randomNum2 = Math.floor(Math.random() * max) + min;
-    randomNum3 = Math.floor(Math.random() * max) + min;
-    label1.textContent= randomNum1;
-    label2.textContent= randomNum2;
-    label3.textContent= randomNum3;
+    }
+    else if(masterCardBtn.checked){
+        paymentResult.textContent = `You are paying with MasterCard`;
 
+    }
+    else if(payPalBtn.checked){
+        paymentResult.textContent = `You are paying with PayPal`;
+    }
+    else{
+        paymentResult.textContent = `You must select a payment type`;
+    }
 }
