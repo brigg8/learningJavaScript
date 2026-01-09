f14e1a0011533c51555f3c6c01e2b7886d0e3d86
Brandon Riggione <bpr008@bucknell.edu>
2026-01-09 12:15:44 -0500

    Switch statements practice

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-// RANDOM NUMBER GENERATOR
+// SWITCh = can be an efficient replacement to many else if statements
 
-const myButton = document.getElementById("myButton");
-const label1 = document.getElementById("label1");
-const label2 = document.getElementById("label2");
-const label3 = document.getElementById("label3");
+let testScore = 90;
 
-const min = 1;
-const max = 6;
-let randomNum1;
-let randomNum2;
-let randomNum3;
+let letterGrade;
 
-
-myButton.onclick = function(){
-    randomNum1 = Math.floor(Math.random() * max) + min;
-    randomNum2 = Math.floor(Math.random() * max) + min;
-    randomNum3 = Math.floor(Math.random() * max) + min;
-    label1.textContent= randomNum1;
-    label2.textContent= randomNum2;
-    label3.textContent= randomNum3;
-
+switch(true){
+    case testScore >= 90:
+        letterGrade = "A";
+        break;
+    case testScore >= 80:
+        letterGrade = "B";
+        break;
+    case testScore >= 70:
+        letterGrade = "C";
+        break;
+    case testScore >= 60:
+        letterGrade = "D";
+        break;
+    default:
+        letterGrade = "F"
+    
 }
+
+console.log(letterGrade);
