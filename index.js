51b87b6d41cd158c0e62fddfb04143439063ac99
Brandon Riggione <bpr008@bucknell.edu>
2026-01-11 13:36:34 -0500

    Reusable function practice

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-// String methods = allow you to manipulate and work with text (strings)
+// function = A section of reusabl;e code. 
+// Declare code once, use it whenever you want. 
+// Call the function to execute that code.
 
-let phoneNumber = "123-456-7890";
+// function happyBirthday(username, age){
+//     console.log("Happy birthday to you!");
+//     console.log("Happy birthday to you!");
+//     console.log(`Happy birthday dear ${username}!`);
+//     console.log("Happy birthday to you!");
+//     console.log(`You are ${age} years old`);
+// }
 
-// phoneNumber = phoneNumber.replaceAll("-", "/");
+// happyBirthday("Brandon", 25);
+// happyBirthday("Spongebob", 30);
+// happyBirthday("Patrick", 37);
 
-/*
-    padds the start of a strings to make the string 15 characters long
-// phoneNumber = phoneNumber.padStart(15, "0");
-*/
+function add(x, y){
+    // let result = x + y;
+    // return result;
+    return x + y;
+}
 
-/*
-    padds the end of a strings to make the string 15 characters long
-*/
-// phoneNumber = phoneNumber.padEnd(15, "0");
+function subtract(x, y){
+    return x - y;
+}
+function multiply(x, y) {
+    return x * y;
+}
+function divide(x, y) {
+    return x / y;
+}
+function isEven(number){
+    return number % 2 === 0 ? true : false;
+}
+function isValidEmail(email){
+    // if(email.includes("@")) {
+    //     return true;
+    // }
+    // else{
+    //     return false;
+    // }
+    return email.includes("@") ? true : false;
+}
+console.log(isValidEmail("brandonriggione@fakegmail.com"));
 
-
-console.log(phoneNumber);
