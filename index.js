d89710cd9df838479a4857ea37eb6bded2c2f4d9
Brandon Riggione <bpr008@bucknell.edu>
2026-01-11 14:44:32 -0500

    array practice

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
-// String methods = allow you to manipulate and work with text (strings)
+// array = a variable like structure that can hold
+// more than 1 value
 
-let phoneNumber = "123-456-7890";
+let fruits = ["apple", "orange", "banna"];
 
-// phoneNumber = phoneNumber.replaceAll("-", "/");
+// fruits.push("coconut");
+// // push add things to end of array
+// fruits[0] = "coconut";
+// console.log(fruits);
 
-/*
-    padds the start of a strings to make the string 15 characters long
-// phoneNumber = phoneNumber.padStart(15, "0");
-*/
+// fruits.pop();
+// // gets ride of the last element in the array
+// console.log(fruits);
 
-/*
-    padds the end of a strings to make the string 15 characters long
-*/
-// phoneNumber = phoneNumber.padEnd(15, "0");
+// fruits.unshift("mango");
+// //adds element to beggining
+// console.log(fruits);
+// fruits.shift();
+// // gets ride of the first element in an array
+// console.log(fruits);
 
+// let numOfFruits = fruits.length;
+// // Gets the length of an array
+// console.log(numOfFruits);
 
-console.log(phoneNumber);
+// let index = fruits.indexOf("apple");
+// // indexOf finds the index of element. If the element is not there it returns -1
+// console.log(index);
+
+// for(let i = 0; i < fruits.length; i++) {
+//     console.log(i);
+//     console.log(fruits[i]);
+// }
+
+// for(let i = fruits.length - 1; i >= 0; i--) {
+//     console.log(fruits[i]);
+// }
+
+// Enhanced for loop for arrays below:
+
+// for(let fruit of fruits) {
+//     console.log(fruit);
+// }
+
+fruits.sort().reverse();
+
+for(let fruit of fruits) {
+    console.log(fruit);
+}
