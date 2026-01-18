b47238e4ed80ccae743d40cff55d2e534c9a4db4
Brandon Riggione <bpr008@bucknell.edu>
2026-01-18 14:43:47 -0500

    ES6 Modules practice

---
+++ index.js
@@ -0,0 +1,15 @@
+// ES6 Module = An external file that contains reusable code
+//              that can be imported into another JavaScript files.
+//              write reusable code for many different apps.
+//              Can contain variables, classes, functions ... and more
+//              Introduced as part of EXMAScript 2015 update
+
+import {PI, getCircumference, getArea, getVolume} from './mathUtil.js';
+
+console.log(PI);
+const circumference = getCircumference(10);
+const area = getArea(10);
+const volume = getVolume(10);
+console.log(`${circumference.toFixed(2)}cm`);
+console.log(`${area.toFixed(2)}cm^2`);
+console.log(`${volume.toFixed(2)}cm^3`);
