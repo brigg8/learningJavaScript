698a326c9603e77a0608e1ac079fb5f49f5747bf
Brandon Riggione <bpr008@bucknell.edu>
2026-01-18 20:49:52 -0500

    creating HTML elements practice

---
+++ index.js
@@ -0,0 +1,54 @@
+// ----------- Example 1 <h1> -----------
+
+// STEP 1 CREATE THE ELEMENT
+const newH1 = document.createElement("h1");
+
+// STEP 2 ADD ATTRIBUTES/PROPERTIES
+newH1.textContent = "I like pizza";
+newH1.id = "myH1";
+newH1.style.color = "tomato";
+newH1.style.textAlign = "center";
+// STEP 3 APPEND ELEMENT TO DOM
+
+// document.body.append(newH1);
+// document.body.prepend(newH1);
+// document.getElementById("box1").prepend(newH1);
+
+// document.getElementById("box1").append(newH1);
+
+// const box2 = document.getElementById("box2");
+// document.body.insertBefore(newElement, currentElement);
+// document.body.insertBefore(newH1, box2);
+
+// const boxes = document.querySelectorAll(".box");
+// document.body.insertBefore(newH1, boxes[0]);
+
+// REMOVE HTML ELEMENT
+
+// document.getElementById("box1").removeChild(newH1);
+
+
+// ----------- Example 2 <li> -----------
+
+// STEP 1 CREATE THE ELEMENT
+
+const newListItem = document.createElement("li");
+
+// STEP 2 ADD ATTRIBUTES/PROPERTIES
+newListItem.textContent = "coconut";
+newListItem.id = "cocounut";
+newListItem.style.fontWeight = "bold";
+newListItem.style.backgroundColor = "lightgreen";
+
+// STEP 3 APPEND ELEMENT TO DOM
+// document.body.append(newListItem);
+// document.body.prepend(newListItem);
+// const orange = document.getElementById("orange");
+document.getElementById("fruits").append(newListItem);
+// document.getElementById("fruits").insertBefore(newListItem, orange);
+// document.getElementById("fruits").insertBefore(newListItem, orange);
+// const listItems = document.querySelectorAll("#fruits li");
+// document.getElementById("fruits").insertBefore(newListItem, listItems[1]);
+
+// REMOVE HTML ELEMENT
+document.getElementById("fruits").removeChild(newListItem);
